313c79c12d3caa512a233e1a5ec721212dfd868e
KeKe <zkaip1@gmail.com>
2016-04-19 06:39:49 +0800

    demo create store

--- c6-redux-study/demo-create-store/index.js
+++ c6-redux-study/demo-create-store/index.js
@@ -1,51 +1,48 @@
-// reducer 纯函数，具体的action执行逻辑
+// reducer 纯函数, action执行逻辑
 const counter = (state = 0, action) => {
   switch (action.type) {
-      case 'INCREMENT':
-        return state + 1;
-      case 'DECREMENT':
-        return state - 1;
-      default:
-        return state;
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
   }
 }
 
-// 模拟create store，了解其原理
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
+// 模拟createStore
 
-  const getState = () => state;
-
+const createStore = (reducer, state) => {
+  let listeners = []
+  const getState = () => state
   const dispatch = (action) => {
-    state = reducer(state, action);
-    listeners.forEach(listener => listener());
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
   }
-
   const subscribe = (listener) => {
-      listeners.push(listener);
-      return () => {
-        listeners = listeners.filter(item => item !== listener);
-      }
+    listeners.push(listener)
+    return () => {
+      listeners = listener.filter(item => item !== listener)
+    }
   }
-
-  dispatch({});
-
-  return { getState, dispatch, subscribe };
+  
+  dispatch({})
+  
+  return {getState, dispatch, subscribe}
 }
 
-const store = createStore(counter);
+const store = createStore(counter)
 
 // view 对应到React里面的component
 const PureRender = () => {
-  document.body.innerText = store.getState();
+  document.body.innerText = store.getState()
 }
 
 // store subscribe 订阅或是监听view（on）
-store.subscribe(PureRender);
-PureRender();
+store.subscribe(PureRender)
+PureRender()
 
 document.addEventListener('click', function( e ){
   // store dispatch 调度分发一个 action（fire）
-  store.dispatch({ type: 'DECREMENT'});
+  store.dispatch({ type: 'INCREMENT'})
 })
