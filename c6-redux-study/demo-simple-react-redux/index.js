f1e69397632d6b632a1c755891c3b3dd41a1195d
KeKe <zkaip1@gmail.com>
2016-04-19 20:55:39 +0800

    add simple react redux

--- c6-redux-study/demo-simple-react-redux/index.js
+++ c6-redux-study/demo-simple-react-redux/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 
-// 这是一个展示型组件 Counter
+// 展示型组件
 class Counter extends Component {
-  render() {
-    const { value, onIncreaseClick } = this.props
+  render(){
+    const {value, onIncreaseClick, onDecreaseClick} = this.props
     return (
       <div>
         <span>{value}</span>
-        <button onClick={onIncreaseClick}>戳我加1</button>
+        <button onClick={onIncreaseClick}>戳我+1</button>
+        <button onClick={onDecreaseClick}>戳我-1</button>
       </div>
     )
   }
@@ -20,63 +21,61 @@ Counter.propTypes = {
   value: PropTypes.number.isRequired,
   onIncreaseClick: PropTypes.func.isRequired
 }
-
 // Action
-const increaseAction = { type: 'increase' }
+const increaseAction = {type: 'INCREASE'},
+      decreaseAction = {type: 'DECREASE'}
 
 // Reducer
-function counter(state = { count: 0 }, action) {
+function counter (state = { count:0 }, action) {
   let count = state.count
   switch (action.type) {
-    case 'increase':
-      return { count: count + 1 }
+    case 'INCREASE':
+      return {count: count + 1}
+    case 'DECREASE':
+      return {count: count - 1}
     default:
       return state
   }
 }
 
 // Store
-let store = createStore(counter)
+const store = createStore(counter)
 
-// Map Redux state to component props
-function mapStateToProps(state) {
-  // 这里拿到的state就是store里面给的state
-  console.log(state);
+// Map redux state to component props
+function mapStateToProps (state) {
+  // 拿到的state就是store里面的state
   return {
     value: state.count
   }
 }
 
 // Map Redux actions to component props
-function mapDispatchToProps(dispatch) {
-  // dispatch(action) { }
+function mapDispatchToProps (dispatch) {
   return {
-    onIncreaseClick: () => dispatch(increaseAction)
+    onIncreaseClick: () => dispatch(increaseAction),
+    onDecreaseClick: () => dispatch(decreaseAction)
   }
 }
 
 class App extends Component {
-  render() {
-    // store里的state经过connect连接后给了根组件的props
-    console.log(this.props);
+  render(){
+    console.log('props:',this.props)
     return (
       <div>
-        <h1>学习使用react-redux</h1>
-        <Counter {...this.props} />
+        <h1>学习使用 react-redux</h1>
+        <Counter {...this.props}/>
       </div>
     )
   }
 }
 
 // Connected Component
-let RootApp = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
+const RootApp = connect(mapStateToProps,mapDispatchToProps)(App)
 
+// 最终 render
 ReactDOM.render(
   <Provider store={store}>
-    <RootApp />
+    <RootApp/>
   </Provider>,
   document.getElementById('app')
 )
