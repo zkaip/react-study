aa5d109329002d7b108a67931701062109f82aa6
KeKe <zkaip1@gmail.com>
2016-04-20 16:46:06 +0800

    middleware apply

--- c6-redux-study/demo-middleware/middleware/index.js
+++ c6-redux-study/demo-middleware/middleware/index.js
@@ -1,29 +1,26 @@
-/**
- * 记录所有被发起的 action 以及产生的新的 state。
- */
+// 记录所有被发起的 action 以及产生的新的 state。
 export const logger = store => next => action => {
   console.group(action.type)
-  console.info('dispatching', action)
+  console.info('dispatch',action)
+  console.log('now state', store.getState())
   let result = next(action)
   console.log('next state', store.getState())
   console.groupEnd(action.type)
   return result
 }
 
-/**
- * 在 state 更新完成和 listener 被通知之后发送崩溃报告。
- */
+// 在 state 更新完成和 listener 被通知之后发送崩溃报告。
 export const crashReporter = store => next => action => {
   try {
     return next(action)
-  } catch (err) {
-    console.error('Caught an exception!', err)
-    Raven.captureException(err, {
+  } catch (error) {
+    console.error('Caught an exception!', error)
+    Raven.captureException(error,{
       extra: {
         action,
         state: store.getState()
       }
     })
-    throw err
+    throw error
   }
 }
